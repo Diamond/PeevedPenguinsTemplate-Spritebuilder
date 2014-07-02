6b6b106c9021021acaeb3b387ccf4eb039295c57
Brandon Richey <brandon@new-host-6.home>
2014-07-02 00:45:17 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -0,0 +1,25 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Brandon Richey on 7/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+-(void)didLoadFromCCB
+{
+    float delay = (arc4random() % 2000) / 1000.0f;
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+}
+
+-(void)startBlinkAndJump
+{
+    CCAnimationManager *animationManager = self.animationManager;
+    [animationManager runAnimationsForSequenceNamed:@"blinkAndJump"];
+}
+
+@end
